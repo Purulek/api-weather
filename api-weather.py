74b56ffa8942e4dc3b71eb63c6fd42282115ddf4
purulek <adam.purul@gmail.com>
2024-09-23 23:35:01 +0200

    next commit 23.09

--- api-weather.py
+++ api-weather.py
@@ -1,1 +1,33 @@
 import json
+import requests
+import tkinter
+
+
+lat_and_lon =[]
+
+try:
+    r = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=key")
+    handle1 = r.json()
+
+except json.decoder.JSONDecodeError:
+    print("wrong URL")
+
+for key in handle1:
+    print("-"*20)
+    for value in key:
+
+        if type(key[value]) == float:
+            lat_and_lon.append(key[value])
+
+
+
+
+try:
+    r = requests.get("https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid=key".format(lat_and_lon[0],lat_and_lon[1]))
+    handle2 = r.json()
+
+except json.decoder.JSONDecodeError:
+    print("wrong URL")
+
+else:
+    print(handle2)
