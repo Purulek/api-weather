a8c233f3527c8949eb21b75ff84f913dd26ef083
purulek <adam.purul@gmail.com>
2024-09-29 22:30:15 +0200

    next commit 29.09

--- api-weather.py
+++ api-weather.py
@@ -4,7 +4,7 @@ import tkinter as tk
 from tkinter import messagebox
 
 
-
+Key = "XXX"
 lat_and_lon =[]
 temputer = []
 root = tk.Tk()
@@ -12,11 +12,17 @@ root.title("In wich country you want chek tempeture")
 
 
 def cheking_country():
+    global messeg
     messeg = input_text.get()
     if not messeg.strip():
         messagebox.showwarning("error", "pleas give name of country")
-        return
-    return messeg
+        return  
+    else:
+        root.destroy()
+
+    
+    
+    
 
 label =tk.Label(root, text="Wirte name of the country in wich you want chek temeprature:")
 label.pack(padx=10, pady=10)
@@ -29,7 +35,7 @@ root.mainloop()
 
 
 try:
-    r = requests.get("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=1&appid=key")
+    r = requests.get("http://api.openweathermap.org/geo/1.0/direct?q={}&limit=1&appid={}".format(messeg.capitalize(),Key))
     handle1 = r.json()
 
 except json.decoder.JSONDecodeError:
@@ -40,6 +46,7 @@ except json.decoder.JSONDecodeError:
 else:
     for key in handle1:
         for value in key:
+            
 
             if type(key[value]) == float:
                 lat_and_lon.append(key[value])
@@ -48,17 +55,28 @@ else:
 
 
 try:
-    r = requests.get("https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid=key".format(lat_and_lon[0],lat_and_lon[1]))
+    r = requests.get("https://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&appid={}".format(lat_and_lon[0],lat_and_lon[1],Key))
     handle2 = r.json()
+
 
 except json.decoder.JSONDecodeError:
     print("wrong URL")
 
 else:
-    for key in handle1:
-        for value in key:
 
-            if type(key[value]) == 'temp_min' or type(key[value]) ==  'temp_max':
-                temputer.append(key[value])
+    for key in handle2:
+        try:
+            print(handle2[key])
 
-messagebox.showinfo ("Tempeture", temputer)
+            for value in handle2[key]:
+                try:
+                    print(handle2[key][value])
+                except:
+                    pass
+        except:
+            pass
+
+        
+
+            
+    messagebox.showinfo ("Tempeture", temputer)
